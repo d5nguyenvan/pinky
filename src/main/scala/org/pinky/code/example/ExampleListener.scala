7469f81a9a36451a2b234255d47aef616d1419ab
fede <fede.silva@gmail.com>
2009-04-18 17:24:41 -0300

    Module with implicit conversion to change with to by in guice-servlet EDSL. ExampleListener modified to import the module.

--- src/main/scala/org/pinky/code/example/ExampleListener.scala
+++ src/main/scala/org/pinky/code/example/ExampleListener.scala
@@ -7,23 +7,32 @@ import extension.controlstructure._
 import com.google.inject.servlet.{ServletModule, GuiceServletContextListener}
 import com.google.inject.{AbstractModule, Guice, Injector, Module}
 import servlets.{ExampleFilter, ExampleRssServlet, ExampleServlet}
+
 /**
  * Listener example which demonstrates how to configure guice managed filters, servlets and other components the "pinky way"
  *
  * @author peter hausel gmail com (Peter Hausel)
+ *
  */
 
-  class ExampleListener extends PinkyServletContextListener
-  {
+class ExampleListener extends PinkyServletContextListener
+{
 
     modules = Array(
       new RepresentationModule(),
       new ServletModule(){
+
+        // Imports the DSL fixes for scala
+        // Note how we can import it in 
+        //    the precise scope it's gonna be used
+        import org.pinky.code.extension.guice.DSLFixes._
+
         filter("/hello/*").through(classOf[ExampleFilter])
         serve("*.rss").by(classOf[ExampleRssServlet])
         serve("/hello/*").by(classOf[ExampleServlet])
       }
-  )
+
+    )
 
 }
 
---
+++ src/main/scala/org/pinky/code/extension/guice/DSLFixes.scala
@@ -0,0 +1,82 @@
+package org.pinky.code.extension.guice;
+
+
+
+import com.google.inject.Key
+import com.google.inject.servlet._
+
+
+import javax.servlet.http.{HttpServletResponse, HttpServletRequest, HttpServlet}
+
+/**
+ *  Holds implicit definitions to hide the calls to `with`.
+ *  Import this object into the scope in which you need to do
+ *  <code>serve("*.rss").with(classOf[ExampleRssServlet])</code>
+ *  and do
+ *  <code>serve("*.rss").by(classOf[ExampleRssServlet])</code> or
+ *  <code>serve("*.rss").withClass(classOf[ExampleRssServlet])</code>
+ *  instead.
+ *
+ *   This are the methods we will hide
+ *   <code>
+ *   public static interface ServletKeyBindingBuilder {
+ *       void with(Class<? extends HttpServlet> servletKey);
+ *       void with(Key<? extends HttpServlet> servletKey);
+ *       void with(Class<? extends HttpServlet> servletKey, Map<String, String> contextParams);
+ *       void with(Key<? extends HttpServlet> servletKey, Map<String, String> contextParams);
+ *   }
+ *   </code>
+ *
+ * @author fede silva gmail com {fedesilva}
+ *
+ */
+object DSLFixes {
+
+    /**
+     * The rich type.
+     * @author fede silva gmail com {fedesilva}
+     */
+    class RichBuilder( builder: ServletModule.ServletKeyBindingBuilder ){
+
+        def by[T <: HttpServlet]( servletKey: Class[T] ){ builder.`with`(servletKey) };
+
+        def by[T <: HttpServlet](
+            servletKey: Class[T], params: java.util.Map[String, String] ) { builder.`with`(servletKey, params) };
+
+
+        def by[T <: HttpServlet]( servletKey: Key[T] ){ builder.`with`(servletKey) };
+
+        def by[T <: HttpServlet, S](
+            servletKey: Key[T],
+            params: java.util.Map[String, String] ){ builder.`with`(servletKey, params) };
+
+
+        def withClass[T <: HttpServlet]( servletKey: Class[T] ){
+            builder.`with`(servletKey)
+        };
+
+        def withClass[T <: HttpServlet](
+            servletKey: Class[T],
+            params: java.util.Map[String, String] ){ builder.`with`(servletKey, params) };
+
+        def withClass[T <: HttpServlet]( servletKey: Key[T] ){
+            builder.`with`(servletKey)
+        };
+
+        def withClass[T <: HttpServlet](
+            servletKey: Key[T],
+            params: java.util.Map[String, String] ){ builder.`with`(servletKey, params) };
+
+    }
+
+    /**
+     * The implicit type conversion.
+     * @author fede silva gmail com {fedesilva}
+     */
+    implicit def builderToRichBuilder(
+        builder: ServletModule.ServletKeyBindingBuilder ) = new RichBuilder(builder);
+
+    
+
+}
+
