4d96e10edf4be42e7ac3ba763bc25327192a3783
peter hausel <peter_kovac11@hotmail.com>
2010-01-28 00:11:19 -0500

    switch away from Source.io in example

--- project/build/PinkyProject.scala
+++ project/build/PinkyProject.scala
@@ -9,10 +9,11 @@ class PinkyProject(info: ProjectInfo) extends DefaultProject(info) {
     val akka_repo = "Akka Maven Repository" at "http://scalablesolutions.se/akka/repository"
     val guiceMaven = "guice maven repository" at "http://guice-maven.googlecode.com/svn/trunk"
     val jetty_repo = "jetty repository" at "http://oss.sonatype.org/content/groups/jetty"
-    
+
     val akka = "se.scalablesolutions.akka" % "akka-core" % "0.6" % "compile"
     val jettyComet = "org.eclipse.jetty" % "jetty-continuation" % "7.0.2-SNAPSHOT" % "compile" 
-
+    val io = "commons-io" % "commons-io" % "1.5-SNAPSHOT" % "compile"
+    
     val guice = "com.google.code.guice" % "guice" % "2.0.1" % "compile" 
     val guiceServlet = "com.google.code.guice" % "guice-servlet" % "2.0.1" % "compile" 
     val aopalliance = "aopalliance" % "aopalliance" % "1.0" % "compile"
@@ -23,6 +24,7 @@ class PinkyProject(info: ProjectInfo) extends DefaultProject(info) {
     val xstream = "com.thoughtworks.xstream" % "xstream" % "1.3.1" % "compile"
     val json = "org.json" % "json" % "20080701" % "compile"
     val jettison = "org.codehaus.jettison" % "jettison" % "1.1" % "compile"
+    //testing
     val mockito = "org.mockito" % "mockito-core" % "1.6" % "test->default"
     val h2database = "com.h2database" % "h2" % "1.0.20070617" % "test->default"
     val servlet = "javax.servlet" % "servlet-api" % "2.5" % "provided" 
--- src/main/scala/org/pinky/code/example/servlets/ExampleCometServlet.scala
+++ src/main/scala/org/pinky/code/example/servlets/ExampleCometServlet.scala
@@ -8,6 +8,7 @@ import scala.io.Source.fromInputStream
 import org.pinky.code.extension.controlstructure.{Resume, ActorCometClient}
 import org.eclipse.jetty.continuation.Continuation
 import scala.xml._
+import org.apache.commons.io.IOUtils
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,21 +19,25 @@ import scala.xml._
  */
 
 class MyActorCometClient(continuation: Continuation, request: HttpServletRequest)
-            extends ActorCometClient(continuation,request) {
-    val url = new URL("http://twitter.com/statuses/user_timeline/5047741.rss")
-    override def receive = handler {
-      case "readfeed" => {
-        val stuff = for (line <- (XML.loadString(fromInputStream(url.openStream).getLines.mkString) \\ "title")) yield line.text
+        extends ActorCometClient(continuation, request) {
+  val url = new URL("http://twitter.com/statuses/user_timeline/5047741.rss")
+  override def receive = handler {
+    case "readfeed" => {
+      val  in = url.openStream
+      try {
+        val stuff = for (line <- (XML.loadString(IOUtils.toString(in)) \\ "title")) yield line.text
         writer(continuation).println(stuff.mkString("<br>"))
-      }
-    }
-    def callback = {
-      println("about to hit callback")
-      Thread.sleep(2000)
-      this ! "readfeed"
-      println("about to hit second sleep")
-      Thread.sleep(3000)
-      this ! Resume
+      } finally {IOUtils.closeQuietly(in)}
     }
   }
+
+  def callback = {
+    println("about to hit callback")
+    Thread.sleep(1000)
+    this ! "readfeed"
+    println("about to hit second sleep")
+    Thread.sleep(1000)
+    this ! Resume
+  }
+}
 @Singleton class ExampleCometServlet extends CometServlet[MyActorCometClient]
